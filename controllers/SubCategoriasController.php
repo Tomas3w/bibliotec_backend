59bae79d8be3ce3d79cced0a8acdddeb3054a8e7
rafa5551 <rafa5551@hotmail.com>
2023-05-29 11:11:58 -0300

    subcategorias/create implementado

---
+++ controllers/SubCategoriasController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\controllers;
+use app\models\SubCategorias;
+use app\models\Usuarios;
+use app\models\LogAbm;
+use app\models\LogAccion;
+
+class SubCategoriasController extends \yii\web\Controller
+{
+    public $enableCsrfValidation = false;
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionCreate(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $datos = $this->request->bodyParams;
+            $subcat_cat_id = $datos['subcat_cat_id'];
+            $subcat_nombre = $datos['subcat_nombre'];
+            $subcat_vigente = $datos['subcat_vigente'];
+
+            if (!isset($subcat_cat_id) || empty($subcat_cat_id))
+                return json_encode(array("error"=>true, "error_tipo"=>"0","mensaje"=>"No se ha enviado el 'subcat_cat_id'."));
+            if (!isset($subcat_nombre) || empty($subcat_nombre))
+                return json_encode(array("error"=>true, "error_tipo"=>"1","mensaje"=>"No se ha enviado el 'subcat_nombre'."));
+            if (!isset($subcat_vigente) || empty($subcat_vigente))
+                return json_encode(array("error"=>true, "error_tipo"=>"2","mensaje"=>"No se ha enviado el 'subcat_vigente'."));
+            if ($subcat_vigente != 'S' && $subcat_vigente != 'N')
+                return json_encode(array("error"=>true, "error_tipo"=>"3","mensaje"=>"Vigente puede ser 'S' o 'N'."));
+            if (!Usuarios::checkIfAdmin($this->request, $this->modelClass))
+                return json_encode(array("codigo"=>true, "error_tipo"=>"4","mensaje"=>"El token no corresponde a un administrador o no se ha enviado."));
+
+            $subcategoria = SubCategorias::findOne(['subcat_nombre' => $subcat_nombre, 'subcat_cat_id' => $subcat_cat_id]);
+            if ($subcategoria != null)
+                return json_encode(array("error"=>true, "error_tipo"=>"4", "mensaje"=>"La subcategoria con nombre '".$subcat_nombre."' ya esta creada para categoria con id ".$subcat_cat_id));
+
+            // GUARDAR NUEVA CATEGORIA
+            $subcategoriaNueva = SubCategorias::nuevaSubCategoria($subcat_cat_id, $subcat_nombre, $subcat_vigente);
+
+            // CREAR LOG
+            $subcategoriaNuevaJson = null;
+            $nombreTabla = SubCategorias::tableName();
+            $subcategoriaNuevaJson = json_encode($subcategoriaNueva->attributes);
+            $usu_id_admin = Usuarios::findIdentityByAccessToken(Usuarios::getTokenFromHeaders($this->request->headers))->usu_id;
+            $id_logAbm = LogAbm::nuevoLog($nombreTabla,1,NULL,$subcategoriaNuevaJson,"Nueva subcategoria ".$subcat_cat_id, $usu_id_admin);
+            LogAccion::nuevoLog("Nueva subcategoria","Nueva subcategoria '".$subcat_nombre." Agregada a la categoria con id'". $subcat_cat_id."'", $id_logAbm);
+
+            return json_encode(array("error"=>"false","mensaje"=>"Nueva subcategoria creada correctamente."));
+        }
+    }
+
+}
