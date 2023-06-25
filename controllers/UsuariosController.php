0e5cb505eed2216173d5d351d0afb9fa972c200b
rafa5551 <rafa5551@hotmail.com>
2023-06-25 19:41:58 -0300

    update x2

--- controllers/UsuariosController.php
+++ controllers/UsuariosController.php
@@ -232,69 +232,117 @@ class UsuariosController extends \yii\web\Controller
             $datos = $this->request->bodyParams;
 
             if (!Usuarios::checkIfAdmin($this->request, $this->modelClass))
+                return json_encode(array("codigo"=>2));
+
+
+            if (!isset($datos['id']) || empty($datos['id']))
                 return json_encode(array("codigo"=>3));
 
-            if (!isset($datos['id']) || empty($datos['id'])  || !isset($datos['nombre']) || empty($datos['nombre']) )
-                return json_encode(array("codigo"=>2));
-
-            //if (!isset($datos['documento']) || empty($datos['documento']))
-                //return json_encode(array("codigo"=>2));
-
-            if (!isset($datos['apellido']) || empty($datos['apellido']) || !isset($datos['mail']) || empty($datos['mail']) || !isset($datos['clave']) || empty($datos['clave']) )
-                return json_encode(array("codigo"=>2));
-            
-            if (!isset($datos['telefono']) || empty($datos['telefono']) || !isset($datos['activo']) || empty($datos['activo']) || !isset($datos['tipo']) || empty($datos['tipo']) )
-                return json_encode(array("codigo"=>2));
-
-            if (($datos['activo'] != 'S' && $datos['activo'] != 'N'))
-                return json_encode(array("codigo"=>2));
-
-            if (($datos['tipo'] != 1 && $datos['tipo'] != 0))
-                return json_encode(array("codigo"=>2));
-
-
-            // if (!Usuarios::getvalidarCedula($datos['documento'])){
-            //     return json_encode(array("codigo"=>104));
-            //     // El documento es no es valido
-            // }
-
-            if (!filter_var($datos['mail'], FILTER_VALIDATE_EMAIL)){
-                return json_encode(array("codigo"=> 106));
-                // El mail es incorrecto
-            }
-
             $id = $datos['id'];
-
             $usuario = Usuarios::findOne(['usu_id' => $id]);
 
             if ($usuario == null)
                 return json_encode(array("codigo"=>4));
 
-        
-            $usuarioModeloViejo = null;
-            $usuarioModeloNuevo = null;
-            $nombreTabla = Usuarios::tableName();
             $usuarioModeloViejo = json_encode($usuario->attributes);
-            //$usuario->usu_documento = $datos['documento'];
-            $usuario->usu_nombre = $datos['nombre'];
-            $usuario->usu_apellido = $datos['apellido'];
-            $usuario->usu_mail = $datos['mail'];
-            $usuario->usu_clave = Yii::$app->getSecurity()->generatePasswordHash($datos['clave']);
-            $usuario->usu_telefono = $datos['telefono'];
-            $usuario->usu_activo = $datos['activo'];
-            $usuario->usu_tipo_usuario = $datos['tipo'];
+            $bandera = false;
+
+            if (isset($datos['nombre'])){
+                if(empty($datos['nombre'])){
+                    return json_encode(array("codigo"=>5, 'mensaje' => 'El atributo nombre esta vacio'));
+                }else{
+                    $usuario->usu_nombre = $datos['nombre'];
+                    $bandera = true;
+                }
+            }   
+
+            if (isset($datos['apellido'])){
+                if(empty($datos['apellido'])){
+                    return json_encode(array("codigo"=>6, 'mensaje' => 'El atributo apellido esta vacio'));
+                }else{
+                    $usuario->usu_apellido = $datos['apellido'];
+                    $bandera = true;
+                }
+            }
+
+            if (isset($datos['mail'])){
+                if(empty($datos['mail'])){
+                    return json_encode(array("codigo"=>7, 'mensaje' => 'El atributo mail esta vacio'));
+                }else{
+                    if (!filter_var($datos['mail'], FILTER_VALIDATE_EMAIL)){
+                        return json_encode(array("codigo"=> 71, 'mensaje' => 'El mail es incorrecto'));
+                        // El mail es incorrecto
+                    }else{
+                        $usuario->usu_mail = $datos['mail'];
+                        $bandera = true;
+                    }
+ 
+                }
+            } 
             
-            $usuario->save();
+            if (isset($datos['clave'])){
+                if(empty($datos['clave'])){
+                    return json_encode(array("codigo"=>8, 'mensaje' => 'El atributo clave esta vacio'));
+                }else{
+                    $usuario->usu_clave = Yii::$app->getSecurity()->generatePasswordHash($datos['clave']);
+                    $bandera = true;
+                }
+            } 
+            
+            if (isset($datos['telefono'])){
+                if(empty($datos['telefono'])){
+                    return json_encode(array("codigo"=>9, 'mensaje' => 'El atributo telefono esta vacio'));
+                }else{
+                    $usuario->usu_telefono = $datos['telefono'];
+                    $bandera = true;
+                }
+            } 
+            
+            if (isset($datos['activo'])){
+                if(empty($datos['activo'])){
+                    return json_encode(array("codigo"=>10, 'mensaje' => 'El atributo activo esta vacio'));
+                }else{
+                    if (($datos['activo'] != 'S' && $datos['activo'] != 'N')){
+                        return json_encode(array("codigo"=>101, 'mensaje' => 'El atributo activo no cumple la especificacion'));
+                    }else{
+                        $usuario->usu_activo = $datos['activo'];
+                        $bandera = true;
+                    }
+                          
+                }
+            }
+
+            if (isset($datos['tipo'])){
+                if(empty($datos['tipo'])){
+                    return json_encode(array("codigo"=>11, 'mensaje' => 'El atributo tipo esta vacio'));
+                }else{
+                    if (($datos['tipo'] != 1 && $datos['tipo'] != 0)){
+                        return json_encode(array("codigo"=>111, 'mensaje' => 'El atributo tipo no cumple la especificacion'));
+                    }else{
+                        $usuario->usu_tipo_usuario = $datos['tipo'];
+                        $bandera = true;
+                    }
+                          
+                }
+            } 
+
+            if ($bandera){
+                $usuario->save();
+            }else{
+                return json_encode(array("codigo"=>12, 'mensaje' => 'Debe pasar algun atributo para modificarlo'));
+            }
+
             $usuarioModeloNuevo = json_encode($usuario->attributes);
-            
+            $nombreTabla = Usuarios::tableName();
+                  
             $usu_id_admin = Usuarios::findIdentityByAccessToken(Usuarios::getTokenFromHeaders($this->request->headers))->usu_id; // Obtener el id del admin para luego guardar quien hizo la baja
 
             $id_logAbm = LogAbm::nuevoLog($nombreTabla,2,$usuarioModeloViejo,$usuarioModeloNuevo,"Modificar usuario", $usu_id_admin);
             LogAccion::nuevoLog("Modificar usuario","Modificado usuarios con id=".$id, $id_logAbm);
 
-            return json_encode(array("codigo"=>1));       
+            return json_encode(array("codigo"=>13, 'mensaje' => 'Se a modificado con exito'));      
         }else{
-            return json_encode(array("codigo"=>5));
+            return json_encode(array("codigo"=>14, 'mensaje' => 'El metodo no es el correcto'));   
         }
     }
 }
