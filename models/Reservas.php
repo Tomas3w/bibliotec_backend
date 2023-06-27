f9a3723bd899a59d661b69f7a3cb1397b4514376
Tomas3w <88631406+Tomas3w@users.noreply.github.com>
2023-06-27 19:39:01 -0300

    Update Reservas.php

--- models/Reservas.php
+++ models/Reservas.php
@@ -80,12 +80,13 @@ class Reservas extends \yii\db\ActiveRecord
             ->andWhere(['!=', 'resv_id', $this->resv_id])
             ->andWhere(['!=', 'resv_estado', 'X'])
             ->andWhere(['!=', 'resv_estado', 'D'])
-            ->andWhere([
-                'or',
-                ['and', ['>=', 'resv_fecha_hasta', $this->resv_fecha_desde], ['<=', 'resv_fecha_hasta', $this->resv_fecha_hasta]],
-                ['and', ['>=', 'resv_fecha_desde', $this->resv_fecha_desde], ['<=', 'resv_fecha_desde', $this->resv_fecha_hasta]],
-            ])->all()) > 3)
-            $this->addError($attribute, "El usuario especificado ya tiene 3 reservas ya");
+            // ->andWhere([
+            //     'or',
+            //     ['and', ['>=', 'resv_fecha_hasta', $this->resv_fecha_desde], ['<=', 'resv_fecha_hasta', $this->resv_fecha_hasta]],
+            //     ['and', ['>=', 'resv_fecha_desde', $this->resv_fecha_desde], ['<=', 'resv_fecha_desde', $this->resv_fecha_hasta]],
+            // ])
+            ->all()) > 3)
+            $this->addError($attribute, "El usuario especificado tiene 3 reservas ya");
     }
 
     public function reservaIntervalo($attribute, $params)
