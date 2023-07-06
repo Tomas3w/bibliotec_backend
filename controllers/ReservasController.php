a776c6518ca5daba36c34e4449e1a84d3ddaebe6
Tomas3w <88631406+Tomas3w@users.noreply.github.com>
2023-07-06 13:01:05 -0300

    Update ReservasController.php

--- controllers/ReservasController.php
+++ controllers/ReservasController.php
@@ -196,7 +196,7 @@ class ReservasController extends \yii\rest\ActiveController
             array_push($array,$index);
         }
 
-        return ['error' => false, 'reserva' => $array];
+        return ['error' => false, 'reserva' => $array, 'stock' => Libros::findOne(['lib_id' => $id])];
     }
 
 
